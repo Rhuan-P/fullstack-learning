396e78a0961bfba58ba359c96f54212e5741684e
Rhuan Pablo Monteiro Da Silva <rhuanpablomonteiro@gmail.com>
2024-08-05 01:21:17 -0300

    web view page

--- main.py
+++ main.py
@@ -3,6 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():return render_template('index.html')
+def home():
+    # products whith name,price, validity =  valid or expired
+    products = [
+        {"name": "Apple", "price": 10, "validity": "valid"},
+        {"name": "Orange", "price": 20, "validity": "valid"},
+        {"name": "Banana", "price": 30, "validity": "expired"},
+        {"name": "Mango", "price": 40, "validity": "valid"},
+        {"name": "Pineapple", "price": 50, "validity": "expired"},
+        {"name": "Watermelon", "price": 60, "validity": "valid"},
+        {"name": "Grapes", "price": 70, "validity": "expired"},
+
+    ]
+   
+    
+   
+    
+
+    
+    return render_template('index.html', products=products)
 
 app.run(debug=True)
