66ca2a27eb9cc5e89aeffa1626a76f6b6cd38012
Rhuan Pablo Monteiro Da Silva <rhuanpablomonteiro@gmail.com>
2024-08-04 22:00:43 -0300

    drop
    drop

--- main.py
+++ main.py
@@ -3,12 +3,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def Home():
-   user_list = [{'username': 'John', 'age': 30, 'city': 'New', 'state': 'California', 'country': 'USA'},{'username': 'Jane', 'age': 25, 'city': 'Boston', 'state': 'Massachusetts', 'country': 'USA'},{'username': 'Bob', 'age': 40, 'city': 'Seattle', 'state': 'Washington', 'country': 'USA'}]
-   return render_template('index.html',user_list=user_list)
-
-
-
-
+def home():return render_template('index.html')
 
 app.run(debug=True)
