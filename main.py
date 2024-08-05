1a86d19465bc73a9d817b68d386064161ba7d128
Rhuan Pablo Monteiro Da Silva <rhuanpablomonteiro@gmail.com>
2024-08-05 18:12:46 -0300

    feat: Styled table and temporary db

--- main.py
+++ main.py
@@ -1,26 +1,11 @@
 from flask import Flask, render_template
+from db.db import products
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
-def home():
-    # products whith name,price, validity =  valid or expired
-    products = [
-        {"name": "Apple", "price": 10, "validity": "valid"},
-        {"name": "Orange", "price": 20, "validity": "valid"},
-        {"name": "Banana", "price": 30, "validity": "expired"},
-        {"name": "Mango", "price": 40, "validity": "valid"},
-        {"name": "Pineapple", "price": 50, "validity": "expired"},
-        {"name": "Watermelon", "price": 60, "validity": "valid"},
-        {"name": "Grapes", "price": 70, "validity": "expired"},
-
-    ]
-   
-    
-   
-    
-
-    
+def index():
     return render_template('index.html', products=products)
-
 app.run(debug=True)
