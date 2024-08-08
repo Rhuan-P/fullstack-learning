50e15027ad365902cbbe5155b932774af935fd77
Rhuan Pablo Monteiro Da Silva <rhuanpablomonteiro@gmail.com>
2024-08-08 01:09:12 -0300

    feat: GET and POST

--- db/db.py
+++ db/db.py
@@ -1,13 +1,3 @@
 
 
-products = [
-    {"name": "Apple", "price": 10, "validity": "valid"},
-    {"name": "Banana", "price": 5, "validity": "expired"},
-    {"name": "Orange", "price": 15, "validity": "valid"},
-    {"name": "Mango", "price": 20, "validity": "expired"},
-    {"name": "Pineapple", "price": 25, "validity": "valid"},
-    {"name": "Watermelon", "price": 30, "validity": "expired"},
-    
-
-
-]
+products = []
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, url_for, redirect
 from db.db import products
 
 app = Flask(__name__)
@@ -8,4 +8,21 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html', products=products)
+
+@app.route("/register" , methods=["POST"])
+def register():
+    product = {'id': len(products) +1, **request.form}
+    products.append(product)
+    return redirect(url_for('index'))
+
+@app.route("/delete_product/<int:id>", methods=["DELETE"])
+def delete_product(id):
+    
+    return (request.form)
+
+
+
+
+
+
 app.run(debug=True)
